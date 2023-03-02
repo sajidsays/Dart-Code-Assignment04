5a07e090baa640e4c50f328e68b4e462942ae336
sajidsays <sajiafri659@gmail.com>
2023-03-02 21:29:17 +0500

    Fifth Assignment of Dart

---
+++ main.dart
@@ -0,0 +1,109 @@
+import 'dart:io';
+
+void main() {
+  // print("Question # 01");
+  // stdout.write("Enter a number : ");
+  // int num = int.parse(stdin.readLineSync()!);
+
+  // int factorial = 1;
+  // for (int i = 1; i <= num; i++) {
+  //   factorial *= i;
+  // }
+  // print("Factorial of $num : $factorial");
+
+  // print("Question # 02");
+  // List<int> num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+  // int maxNum = num[0];
+  // for (int i = 1; i < num.length; i++) {
+  //   if (num[i] > maxNum) {
+  //     maxNum = num[i];
+  //   }
+  // }
+  // print("The highest number : $maxNum");
+
+  // print("Question # 03");
+  // int num = 0;
+
+  // for (int i = 1; i <= 10; i++) {
+  //   print(num);
+  //   num += 3;
+  // }
+
+  // print("Question # 04 (i)");
+  // for (int i = 1; i <= 4; i++) {
+  //   print("**********");
+  // }
+
+  // print("Question # 04 (ii)");
+  // for (int j = 1; j <= 5; j++) {
+  //   for (int k = 0; k < j; k++) {
+  //     stdout.write("*");
+  //   }
+  //   print("");
+  // }
+
+  // print("Qustion # 04 (iii) is same as above part (ii)");
+
+  // print("Qustion # 04 (iv)");
+  // for (int l = 1; l <= 5; l++) {
+  //   for (int m = 0; m < l * 2 - 1; m++) {
+  //     stdout.write("*");
+  //   }
+  //   print("");
+  // }
+
+  // print("Qustion # 04 (v)");
+  // for (int l = 1; l <= 5; l++) {
+  //   for (int m = 0; m < l * 2 - 1; m++) {
+  //     stdout.write(l);
+  //   }
+  //   print("");
+  // }
+
+  // print("Question # 05");
+  // int sum = 0;
+  // for (int i = 1; i <= 100; i += 2) {
+  //   sum += i;
+  // }
+  // print("The Sum Of All Odd Numbers Between 1 to 100 : $sum");
+
+  // print("Question # 06");
+  // List<int> numbers = [12, 34, 56, 16, 89, 67, 43];
+  // List<int> reversedNumbers = [];
+
+  // for (int i = numbers.length - 1; i >= 0; i--) {
+  //   reversedNumbers.add(numbers[i]);
+  // }
+
+  // print('Original list: $numbers');
+  // print('Reversed list: $reversedNumbers');
+
+  // print("Qustion # 07");
+  // int first = 0, second = 1, next = 0;
+  // print('$first, $second');
+
+  // for (int i = 0; next < 100; i++) {
+  //   next = first + second;
+  //   first = second;
+  //   second = next;
+  //   if (next > 100) {
+  //     break;
+  //   }
+  //   print('$next');
+  // }
+
+  // print("Question # 08");
+  // List<int> numbers = [12, 34, 56, 16, 89, 67, 43];
+
+  // for (int i = 0; i < numbers.length - 1; i++) {
+  //   for (int j = i + 1; j < numbers.length; j++) {
+  //     if (numbers[i] > numbers[j]) {
+  //       int temp = numbers[i];
+  //       numbers[i] = numbers[j];
+  //       numbers[j] = temp;
+  //     }
+  //   }
+  // }
+
+  // print('Sorted list: $numbers');
+}
